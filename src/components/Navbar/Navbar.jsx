23eee0894b4cdd0dbd87ba138da04568d583fae8
chike <chikeegonu@gmail.com>
2024-09-28 14:20:25 +0100

    live update check

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         &nbsp;
         <a href="#" onClick={(e) => handleLinkClick(e.target.innerText)}>Projects</a>
         &nbsp;
-        <a href="#" onClick={(e) => handleLinkClick(e.target.innerText)}>Certifications</a>
+        <a href="#" onClick={(e) => handleLinkClick(e.target.innerText)}>Certification</a>
       </div>
 
       {/* Action Icons */}
